2d9a97f68f7af63ad4c963cb57a3b854ecb39dc1
TacticalTruth <123516809+TacticalTruth@users.noreply.github.com>
2023-03-06 15:03:56 -0600

    base for front&backend practice

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hotel Site' });
+});
+
+router.get('/all', function(req, res){
+  res.render('all_hotels', {title: 'All Hotels'});
 });
 
 module.exports = router;
